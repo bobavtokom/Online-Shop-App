b7ca6e81d15016afc4b2283f6c2366ddd8fb61a1
Boban Milosevik <bobavtokom888@gmail.com>
2023-09-21 23:16:18 +0200

    dashboard category page added

--- Ecommerce/Controllers/AdminController.cs
+++ Ecommerce/Controllers/AdminController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Ecommerce.DAL;
+using Ecommerce.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,19 @@ namespace Ecommerce.Controllers
 {
     public class AdminController : Controller
     {
+        public GenericUnitOfWork unitOfWork = new GenericUnitOfWork();
         // GET: Admin
         public ActionResult Dashboard()
         {
             return View();
+        }
+        public ActionResult Categories() {
+           List<Tbl_Category> allCategories = unitOfWork.GetRepositoryInstance<Tbl_Category>()
+                .GetAllRecordsIQueryable()
+                .Where(i => i.IsDelete == false)
+                .ToList();
+
+            return View(allCategories);
         }
     }
 }

c473357a099951cdab56835b1dcfd8d5e258345a
Boban Milosevik <bobavtokom888@gmail.com>
2023-10-17 22:25:54 +0200

    pagination added on index page

--- Ecommerce/Models/Home/HomeIndexViewModel.cs
+++ Ecommerce/Models/Home/HomeIndexViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Ecommerce.DAL;
 using Ecommerce.Repository;
+using PagedList;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -12,13 +13,13 @@ namespace Ecommerce.Models.Home {
         public static GenericUnitOfWork unitOfWork = new GenericUnitOfWork();
         public dbMyOnlineShoppingEntities context = new dbMyOnlineShoppingEntities();
 
-        public IEnumerable<Tbl_Product> ListOfProducts { get; set; }
-       public HomeIndexViewModel CreateModel(string search) {
+        public IPagedList<Tbl_Product> ListOfProducts { get; set; }
+       public HomeIndexViewModel CreateModel(string search, int pageSize, int? page) {
             SqlParameter[] param = new SqlParameter[] {
                 new SqlParameter(
                     "@search", search??(object)DBNull.Value)
             };
-            IEnumerable<Tbl_Product> data = context.Database.SqlQuery<Tbl_Product>("GetBySearch @search", param).ToList();
+            IPagedList<Tbl_Product> data = context.Database.SqlQuery<Tbl_Product>("GetBySearch @search", param).ToList().ToPagedList(page?? 1,pageSize);
             return new HomeIndexViewModel {
                 ListOfProducts = data
             };

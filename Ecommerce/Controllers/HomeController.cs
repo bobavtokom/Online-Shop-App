89d9b6915f984a3078f37e5e1078d0f0b2b3232b
Boban Milosevik <bobavtokom888@gmail.com>
2023-10-19 22:24:02 +0200

    new features added on index page:

--- Ecommerce/Controllers/HomeController.cs
+++ Ecommerce/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Ecommerce.Models.Home;
+﻿using Ecommerce.DAL;
+using Ecommerce.Models.Home;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,14 +8,21 @@ using System.Web.Mvc;
 
 namespace Ecommerce.Controllers {
     public class HomeController : Controller {
+        public dbMyOnlineShoppingEntities context = new dbMyOnlineShoppingEntities();
+
         public ActionResult Index(string search, int? page) {
             HomeIndexViewModel model = new HomeIndexViewModel();
             return View(model.CreateModel(search, 4, page));
         }
 
-        public ActionResult About() {
-            ViewBag.Message = "Your application description page.";
-
+        public ActionResult AddToCart(int productId) {
+            var cart = new List<Item>();
+            var product = context.Tbl_Product.Find(productId);
+            cart.Add(new Item (){
+                product = product,
+                Quantity = 1
+            });
+            Session["Cart"] = cart;
             return View();
         }
 
---
+++ Ecommerce/Models/Home/Item.cs
@@ -0,0 +1,12 @@
+﻿using Ecommerce.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ecommerce.Models.Home {
+    public class Item {
+        public Tbl_Product product {  get; set; }
+        public int Quantity {  get; set; }
+    }
+}
